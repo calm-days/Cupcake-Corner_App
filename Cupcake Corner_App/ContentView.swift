9bb1ce3d25386d7d56f9397fe214adb20a7cf3bc
Roman Liukevich <r.liukevich@gmail.com>
2022-11-23 18:37:11 +0100

    Initial Commit

---
+++ Cupcake Corner_App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Cupcake Corner_App
+//
+//  Created by Roman Liukevich on 11/23/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Cupcake Corner_App/Cupcake_Corner_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cupcake_Corner_AppApp.swift
+//  Cupcake Corner_App
+//
+//  Created by Roman Liukevich on 11/23/22.
+//
+
+import SwiftUI
+
+@main
+struct Cupcake_Corner_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
